d75de85a53cadf0226e67c0a25108bc845abedd8
Pandidid <74009140+Pandidid@users.noreply.github.com>
2021-10-13 12:28:50 +0800

    Update main.php

--- main.php
+++ main.php
@@ -110,7 +110,7 @@ if(strpos($message, "/cmds") === 0 || strpos($message, "!cmds") === 0){
 <b>/bin | !bin</b> - Bin Lookup
 <b>/iban | !iban</b> - IBAN Checker
   
-  <b>ϟ Join <a href='t.me/IndianBots'>IndianBots</a></b>",
+  <b>ϟ Created by <a href='t.me/LordCireee'>LordCireee</a></b>",
     'parse_mode'=>'html',
     'disable_web_page_preview'=>true,
     'reply_markup'=>json_encode(['inline_keyboard'=>[
